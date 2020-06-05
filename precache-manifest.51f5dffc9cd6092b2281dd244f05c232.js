ef8492855a76bac05b0ce43d825377e0320a767c
nguyễn thiện tâm <nguyentam2112015@gmail.com>
2020-06-05 08:40:14 +0700

    Updates

---
+++ precache-manifest.51f5dffc9cd6092b2281dd244f05c232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164387f069b2984d64df4165ef2ef443",
+    "url": "/algolia/index.html"
+  },
+  {
+    "revision": "b06494ea195cf565eb5c",
+    "url": "/algolia/static/css/main.d9ad9387.chunk.css"
+  },
+  {
+    "revision": "bfc202f382f4f4d825fc",
+    "url": "/algolia/static/js/2.10b80202.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/algolia/static/js/2.10b80202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06494ea195cf565eb5c",
+    "url": "/algolia/static/js/main.e93fc29f.chunk.js"
+  },
+  {
+    "revision": "c1eda75c227f481a2d30",
+    "url": "/algolia/static/js/runtime-main.649adc4d.js"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/algolia/static/media/logo.9f0cccc1.svg"
+  }
+]);
